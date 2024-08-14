775b247fe51f4ba24ccc399688ad56504fe2eac1
leonard <leonard@qq.com>
2024-08-14 18:11:29 +0800

    feat:平台参数新增和vo

--- gulimall-product/src/main/java/com/zljin/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/zljin/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.zljin.gulimall.product.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.zljin.gulimall.product.dao.AttrAttrgroupRelationDao;
+import com.zljin.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import com.zljin.gulimall.product.vo.AttrVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -16,6 +21,10 @@ import com.zljin.gulimall.product.service.AttrService;
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
 
+
+    @Autowired
+    private AttrAttrgroupRelationDao attrAttrgroupRelationDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrEntity> page = this.page(
@@ -25,5 +34,22 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
 
         return new PageUtils(page);
     }
+
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtil.copyProperties(attr, attrEntity);
+        //1. 保存基本数据
+        this.save(attrEntity);
+
+        //2. 如果分组id不为空，说明是规格参数而不是销售属性，则对属性-分组表进行更新
+        if (attr.getAttrGroupId() != null) {
+            AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = new AttrAttrgroupRelationEntity();
+            attrAttrgroupRelationEntity.setAttrGroupId(attr.getAttrGroupId());
+            attrAttrgroupRelationEntity.setAttrId(attrEntity.getAttrId());
+            attrAttrgroupRelationDao.insert(attrAttrgroupRelationEntity);
+        }
+    }
+
 
 }
