c4c6d2d54f5839ba1132a25ee8deef756481b7bd
leonard <leonard@qq.com>
2024-08-16 17:39:19 +0800

    商品优惠卷，保存满减信息api

--- gulimall-coupon/src/main/java/com/zljin/gulimall/coupon/service/SkuFullReductionService.java
+++ gulimall-coupon/src/main/java/com/zljin/gulimall/coupon/service/SkuFullReductionService.java
@@ -1,6 +1,7 @@
 package com.zljin.gulimall.coupon.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zljin.gulimall.common.to.SkuReductionTo;
 import com.zljin.gulimall.common.utils.PageUtils;
 import com.zljin.gulimall.coupon.entity.SkuFullReductionEntity;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface SkuFullReductionService extends IService<SkuFullReductionEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveSkuReduction(SkuReductionTo reductionTo);
 }
 
