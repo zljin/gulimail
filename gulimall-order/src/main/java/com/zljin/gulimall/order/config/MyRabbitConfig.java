38ff94deed8afe3b9ed1b7667d6e4e7ad3aaaf9b
leonard <leonard@qq.com>
2024-09-03 20:32:46 +0800

    add page and integration rabbitmq

---
+++ gulimall-order/src/main/java/com/zljin/gulimall/order/config/MyRabbitConfig.java
@@ -0,0 +1,58 @@
+package com.zljin.gulimall.order.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Slf4j
+@Configuration
+public class MyRabbitConfig {
+
+    RabbitTemplate rabbitTemplate;
+
+    /**
+     * 定制RabbitTemplate
+     * @param connectionFactory
+     * @return
+     */
+    @Primary
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        initRabbitTemplate();
+        return rabbitTemplate;
+    }
+
+    /**
+     * 使用JSON序列化机制，进行消息转换
+     */
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    public void initRabbitTemplate(){
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                log.info("confirm...correlationData[{}]==>ack[{}]==>cause[{}]",correlationData,ack,cause);
+            }
+        });
+
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                log.info("Fail Message[{}]==>replyCode[{}]==>replyText[{}]===>exchange[{}]===>routingKey[{}]",message,replyCode,replyText,exchange,routingKey);
+            }
+        });
+    }
+}
