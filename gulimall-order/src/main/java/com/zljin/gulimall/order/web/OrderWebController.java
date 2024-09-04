544fe8d250605e0e5f2f836ae524cdd25740c967
leonard <leonard@qq.com>
2024-09-04 16:38:59 +0800

    submitOrder and 防重令牌保证幂等性操作 and 本地添加事务注解

--- gulimall-order/src/main/java/com/zljin/gulimall/order/web/OrderWebController.java
+++ gulimall-order/src/main/java/com/zljin/gulimall/order/web/OrderWebController.java
@@ -1,9 +1,11 @@
 package com.zljin.gulimall.order.web;
 
+import com.zljin.gulimall.common.exception.NoStockException;
 import com.zljin.gulimall.order.service.OrderService;
 import com.zljin.gulimall.order.vo.OrderConfirmVo;
 import com.zljin.gulimall.order.vo.OrderSubmitVo;
 import com.zljin.gulimall.order.vo.SubmitOrderResponseVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Controller
 public class OrderWebController {
 
@@ -22,12 +25,13 @@ public class OrderWebController {
     OrderService orderService;
 
     @GetMapping("/{page}.html")
-    public String listPage(@PathVariable("page") String page){
+    public String listPage(@PathVariable("page") String page) {
         return page;
     }
 
     /**
      * 去结算确认页
+     *
      * @param model
      * @param request
      * @return
@@ -36,8 +40,8 @@ public class OrderWebController {
      */
     @GetMapping("/toTrade")
     public String toTrade(Model model, HttpServletRequest request) throws ExecutionException, InterruptedException {
-        OrderConfirmVo confirmVo =  orderService.confirmOrder();
-        model.addAttribute("orderConfirmData",confirmVo);
+        OrderConfirmVo confirmVo = orderService.confirmOrder();
+        model.addAttribute("orderConfirmData", confirmVo);
         //展示订单确认的数据
         return "confirm";
     }
@@ -45,11 +49,39 @@ public class OrderWebController {
 
     /**
      * 下单功能
+     *
      * @param vo
      * @return
      */
     @PostMapping("/submitOrder")
-    public String submitOrder(OrderSubmitVo vo, Model model, RedirectAttributes redirectAttributes){
-        return  "pay";
+    public String submitOrder(OrderSubmitVo vo, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            SubmitOrderResponseVo responseVo = orderService.submitOrder(vo);
+            if (responseVo.getCode() == 0) {
+                //下单成功来到支付选择页
+                model.addAttribute("submitOrderResp", responseVo);
+                return "pay";
+            } else {
+                String msg = "下单失败；";
+                switch (responseVo.getCode()) {
+                    case 1:
+                        msg += "订单信息过期，请刷新再次提交";
+                        break;
+                    case 2:
+                        msg += "订单商品价格发生变化，请确认后再次提交";
+                        break;
+                    case 3:
+                        msg += "库存锁定失败，商品库存不足";
+                        break;
+                    default:
+                }
+                redirectAttributes.addFlashAttribute("msg", msg);
+            }
+        } catch (NoStockException e) {
+            redirectAttributes.addFlashAttribute("msg", e.getMessage());
+        } catch (Exception e) {
+            log.error("error:{}", e.toString());
+        }
+        return "redirect:http://order.gulimall.com/toTrade";
     }
 }
