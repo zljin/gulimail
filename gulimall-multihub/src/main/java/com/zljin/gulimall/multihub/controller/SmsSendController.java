8894b4b87af61c9c811c229ec99f90c2caef2a11
leonard <leonard@qq.com>
2024-12-10 20:14:30 +0800

    @Leonard logback integration elk by logstash

--- gulimall-multihub/src/main/java/com/zljin/gulimall/multihub/controller/SmsSendController.java
+++ gulimall-multihub/src/main/java/com/zljin/gulimall/multihub/controller/SmsSendController.java
@@ -1,18 +1,22 @@
 package com.zljin.gulimall.multihub.controller;
 
 import com.zljin.gulimall.common.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/sms")
 public class SmsSendController {
 
     @GetMapping("/sendcode")
     public R sendCode(@RequestParam("phone") String phone, @RequestParam("code") String code){
-        System.out.println(phone+"===>"+code);
+        log.info(phone+"===>"+code);
+        log.debug(phone+"===>"+code);
+        log.warn(phone+"===>"+code);
         return R.ok();
     }
 }
